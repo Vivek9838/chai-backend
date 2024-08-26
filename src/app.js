c9b2f61b9afce026bc6e1cc9c9c5246acfb5720d
VIVEK <shsrna41@gmail.com>
2024-08-26 17:28:11 +0530

    utils code

---
+++ src/app.js
@@ -0,0 +1,19 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+const app = express();
+
+
+app.use(cors({
+    origin:process.env.CORS_ORGIN,
+    credentials:true
+}))
+app.use(express.json({limit:"16kb"}))//form bhara tb deta liya
+app.use(express.urlencoded({extended:true,limit:"16kb"}))
+app.use(express.static("public")) //koi bhi access kr lega jaise image rakha ho
+app.use(cookieParser())
+
+
+
+export {app};
